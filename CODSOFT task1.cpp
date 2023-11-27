990c55839cd77cf6f1956f3338527c2a6c0da1c9
chith-123 <143883777+chith-123@users.noreply.github.com>
2023-11-27 14:28:19 +0530

    Add files via upload

---
+++ CODSOFT task1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>   // For rand() and srand() functions
+#include <ctime>     // For time() function
+
+using namespace std;
+
+int main() {
+    // Seed the random number generator with the current time
+    srand(time(0));
+
+    // Generate a random number between 1 and 100
+    int secretNumber = rand() % 100 + 1;
+
+    int guess;
+    int attempts = 0;
+
+    cout << "Welcome to the Number Guessing Game!\n";
+    cout << "Try to guess the number between 1 and 100.\n";
+
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+        attempts++;
+
+        if (guess > secretNumber) {
+            cout << "Too high! Try again.\n";
+        } else if (guess < secretNumber) {
+            cout << "Too low! Try again.\n";
+        } else {
+            cout << "Congratulations! You guessed the number in " << attempts << " attempts.\n";
+        }
+
+    } while (guess != secretNumber);
+
+    return 0;
+}
